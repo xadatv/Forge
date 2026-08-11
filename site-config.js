6a1d3968ce112e4d200c60d8908320a93d665a44
XaDa <xadatv@gmail.com>
2026-08-11 14:51:44 +0200

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -20,9 +20,8 @@ window.SITE_CONFIG = {
   storeUrl: 'https://jinxxy.com/XaDa',
   carouselDelay: 10000,
   products: [
-    { name: 'Viking Axe', label: 'FREE ASSET', description: 'A Norse-inspired Viking Axe!', url: 'https://jinxxy.com/XaDa/Viking_Axe', image: '', rune: 'ᛏ' },
-    { name: 'Viking Axe', label: 'FREE ASSET', description: 'A Norse-inspired Viking Axe!', url: 'https://jinxxy.com/XaDa/Viking_Axe', image: 'assets/viking-axe.png', rune: 'ᛏ' },
-    { name: 'Viking Shield', label: 'FREE ASSET', description: 'A Norse-inspired Viking Shield!', url: 'https://jinxxy.com/XaDa/Viking_Shield', image: '', rune: 'ᛟ' }
+    { name: 'Viking Axe', label: 'FREE ASSET', description: 'A Norse-inspired Viking Axe!', url: 'https://jinxxy.com/XaDa/Viking_Axe', image: 'assets/Viking-Axe.png', rune: 'ᛏ' },
+    { name: 'Viking Shield', label: 'FREE ASSET', description: 'A Norse-inspired Viking Shield!', url: 'https://jinxxy.com/XaDa/Viking_Shield', image: 'assets/Viking-Shield.png', rune: 'ᛟ' }
   ],
   links: [
     { title: 'YouTube', subtitle: 'Videos & content', icon: '▶', url: 'https://www.youtube.com/@xadatv' },

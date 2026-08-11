b3a55059bf65d004bb2feabfeb2fd534fa870836
xadatv <xadatv@gmail.com>
2026-08-11 09:43:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const products = [...document.querySelectorAll('.product')];
+const dots = [...document.querySelectorAll('.dot')];
+let current = 0;
+let timer;
+
+function showSlide(index) {
+  current = index;
+  products.forEach((p,i) => p.classList.toggle('active', i === index));
+  dots.forEach((d,i) => d.classList.toggle('active', i === index));
+}
+
+function restart() {
+  clearInterval(timer);
+  timer = setInterval(() => showSlide((current + 1) % products.length), 4200);
+}
+
+dots.forEach((dot, i) => {
+  dot.addEventListener('click', () => {
+    showSlide(i);
+    restart();
+  });
+});
+
+showSlide(0);
+restart();
